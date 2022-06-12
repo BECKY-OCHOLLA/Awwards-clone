22ff373ee2e15f97f75e6b72531190a425164952
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-13 00:11:02 +0300

    it will add signup view

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .models import Profile, Project, Rates
+from django.contrib.auth.models import User
+# from awwardsApp.forms import ProjectForm,RatingsForm,SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 # Create your views here.
 def index(request):
     profile = Profile.objects.all()
