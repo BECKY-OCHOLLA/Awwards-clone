4e8dd384d1f3a6c7393772acac7ced7025a83a05
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-13 00:46:42 +0300

    it will make models imports

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -94,6 +94,33 @@ def profile(request):
     projects = Project.objects.filter(user=current_user.id).all
     return render(request, 'registration/profile.html', {"projects": projects})
 
+@login_required(login_url='/accounts/login/')
+def update_profile(request, id):
+    profile_object = get_object_or_404(Profile, user_id=id)
+    user_object = get_object_or_404(User, id=id)
+    profile_form = UpdateProfileForm(request.POST or None, request.FILES, instance=profile_object)
+    user_form = UpdateUserForm(request.POST or None, instance=user_object)
+    if profile_form.is_valid() and user_form.is_valid():
+        profile_form.save()
+        user_form.save()
+        return HttpResponseRedirect("/profile")
+
+    return render(request, "registration/update_profile.html", {"form": profile_form, "form2": user_form})
+
+@login_required(login_url='/accounts/login')
+def post_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            post_project = form.save(commit=False)
+            post_project.user = current_user
+            post_project.save()
+            return redirect('index')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects.html', {"form": form})
+
 
 
 
