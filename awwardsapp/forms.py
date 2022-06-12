eff39ee36d0ad6f0bed750aee1f8e4db67b951dc
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-13 01:39:53 +0300

    it will add,project form,rating form, update profile form

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -1,4 +1,4 @@
-from awwardsapp.models import Profile, Project, Rates
+from .models import Profile, Project, Rates
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -11,3 +11,26 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['contact','profile_pic', 'bio']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user', 'profile', 'date']
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rates
+        fields = ['design', 'usability', 'content']
