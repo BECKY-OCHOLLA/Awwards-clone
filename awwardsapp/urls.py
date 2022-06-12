dacc7b81e310ea40fea3e03a9c531a502c68ebae
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-13 00:08:51 +0300

    it will add the signup view

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.index,name = 'index'),
+    path('search/', views.search, name='search'),
     path('signup/', views.signup, name='signup'),
     path('profile/',views.profile, name='profile'),
     path('update/<id>', views.update_profile, name='update_profile'),
