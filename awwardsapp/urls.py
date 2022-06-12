925f1e7ef60ec5c3c3d83e177c86dc21080a870e
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-12 23:38:32 +0300

    it will add urls for for,the index page,signup,profile,update,post and project

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -3,3 +3,14 @@ from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    path('',views.index,name = 'index'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/',views.profile, name='profile'),
+    path('update/<id>', views.update_profile, name='update_profile'),
+    path('post/',views.post_project,name='post_project'),
+    path('project/(<id>', views.view_project, name='view_project'),
+
+]
+    
