0549fed629001128d731b75a3269011cb60ceb6d
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-13 10:08:33 +0300

    it will add and updateprojects template

--- awwards/settings.py
+++ awwards/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awwardsapp',
+    'bootstrap3',
+    'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
